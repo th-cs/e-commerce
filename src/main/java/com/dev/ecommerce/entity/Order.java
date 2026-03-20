61b05fbd5ba17ab101ea3de38ecf388477358cb1
th-cs <202718515+th-cs@users.noreply.github.com>
2026-03-20 05:00:53 -0300

    feat: Add Order entity

---
+++ src/main/java/com/dev/ecommerce/entity/Order.java
@@ -0,0 +1,46 @@
+package com.dev.ecommerce.entity;
+
+import com.dev.ecommerce.enums.OrderStatus;
+import java.time.Instant;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.CascadeType;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Instant moment;
+
+	@Column(nullable = false)
+	private OrderStatus status;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@OneToOne(
+	mappedBy = "order",
+	cascade = CascadeType.ALL)
+	private Payment payment;
+}
