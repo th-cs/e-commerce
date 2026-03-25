9e743a460d8012b8cad2945cef2860820dc6ba7d
th-cs <202718515+th-cs@users.noreply.github.com>
2026-03-25 05:18:40 -0300

    refactor: Add ManyToOne annotation

--- src/main/java/com/dev/ecommerce/entity/OrderItem.java
+++ src/main/java/com/dev/ecommerce/entity/OrderItem.java
@@ -5,7 +5,10 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Column;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Getter;
@@ -28,5 +31,15 @@ public class OrderItem {
 
 	@Column(nullable = false)
 	private double price;
+
+	@ManyToOne
+	@MapsId("orderId")
+	@JoinColumn(name = "order_id")
+	private Order order;
+
+	@ManyToOne
+	@MapsId("productId")
+	@JoinColumn(name = "product_id")
+	private Product product;
 
 }
